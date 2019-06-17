01534d56b767324bdc2e06dc353d14071256f48f
drumath <2237k.drumath@gmail.com>
2019-06-17 15:29:20 +0900

    ボーンのメンバーを追加

--- Assets/Script/VRMAvatarController.cs
+++ Assets/Script/VRMAvatarController.cs
@@ -4,6 +4,35 @@ using UnityEngine;
 
 public class VRMAvatarController : MonoBehaviour
 {
+    [SerializeField] GameObject head;
+    [SerializeField] GameObject neck;
+    [SerializeField] GameObject spine_shoulder;
+    [SerializeField] GameObject upper_chest;
+    [SerializeField] GameObject lower_chest;
+    [SerializeField] GameObject spine;
+
+    [SerializeField] GameObject shoulder_right;
+    [SerializeField] GameObject shoulder_left;
+
+    [SerializeField] GameObject elbow_right;
+    [SerializeField] GameObject elbow_left;
+
+    [SerializeField] GameObject hand_right;
+    [SerializeField] GameObject hand_left;
+
+    [SerializeField] GameObject hip_right;
+    [SerializeField] GameObject hip_left;
+
+    [SerializeField] GameObject knee_right;
+    [SerializeField] GameObject knee_left;
+
+    [SerializeField] GameObject foot_right;
+    [SerializeField] GameObject foot_left;
+
+    [SerializeField] GameObject toe_right;
+    [SerializeField] GameObject toe_left;
+
+
     // Start is called before the first frame update
     void Start()
     {
