455f91f8f3d10a09967d5477357389faae1f2d19
drumath <2237k.drumath@gmail.com>
2019-06-17 23:26:11 +0900

    boneの割り当て。ちょっと想像と違う結果に...

--- Assets/Script/VRMAvatarController.cs
+++ Assets/Script/VRMAvatarController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Kinect = Windows.Kinect;
 
 public class VRMAvatarController : MonoBehaviour
 {
@@ -45,6 +46,25 @@ public class VRMAvatarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _BodyManager = BodySourceManager.GetComponent<BodySourceManager>();
+        Kinect.Body[] data = _BodyManager.GetData();
+        var _body = data[0];
+
+        if (_body.IsTracked)
+        {
+            //head.transform.position = GetVector3FromJoint(_body.Joints[Kinect.JointType.Head]);
+            //neck.transform.position = GetVector3FromJoint(_body.Joints[Kinect.JointType.Neck]);
+            //spineShoulder.transform.position = GetVector3FromJoint(_body.Joints[Kinect.JointType.SpineShoulder]);
+            //upperChest.transform.position = GetVector3FromJoint(_body.Joints[Kinect.JointType.SpineMid]);
+            //lowerChest.transform.position = GetVector3FromJoint(_body.Joints[Kinect.JointType.SpineBase]);
+            spine.transform.position = GetVector3FromJoint(_body.Joints[Kinect.JointType.SpineBase]);
+        }
         
     }
+
+    private static Vector3 GetVector3FromJoint(Kinect.Joint joint)
+    {
+        return new Vector3(joint.Position.X * 10, joint.Position.Y * 10, joint.Position.Z * 10);
+    }
+
 }
